a92e01decb84992e6a1fb041f1940d39829c8afa
Joshua Tracy <joshua.tracy@hashicorp.com>
2024-02-18 18:37:07 -0500

    adding multiple directories

---
+++ deployments/dev/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "mordor2-terraform-state" # Use the appropriate bucket name for each project
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "mordor-terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ssm_parameter" "example_param" {
+  name  = "/config/dorinsday/dev-password" 
+  type  = "SecureString"
+  value = "Poootaaaattooees"
+
+
+  tags = {
+    Environment = "Middle Earth"
+}
+}
