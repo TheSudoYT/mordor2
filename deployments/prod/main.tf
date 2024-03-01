cc7e6e89c6e1fbc8b42ecb734442253f05ad34d7
josh tracy <jtracy@email.com>
2024-03-01 09:24:07 -0500

    Remove Terraform backend configuration

--- deployments/prod/main.tf
+++ deployments/prod/main.tf
@@ -1,11 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "mordor2-terraform-state" # Use the appropriate bucket name for each project
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "mordor-terraform-state-lock"
-    encrypt        = true
-  }
+  
 }
 
 provider "aws" {
